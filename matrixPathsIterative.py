503b144377acca8d4277bff5c3b89768ede1dc5d
hshantanu <2hshantanu@gmail.com>
2017-09-25 18:51:17 -0400

    Added the main and other functions.

--- matrixPathsIterative.py
+++ matrixPathsIterative.py
@@ -4,18 +4,24 @@ import numpy as np
 rows = 5
 columns = 8
 
-matrix = np.zeros((rows, columns))
-costMatrix = np.zeros((rows, columns))
-#matrix = np.random.randint(2, size=(5, 7))
+def setUpMatrix():
+	global rows, columns	
+	matrix = np.zeros((rows, columns))
+	#matrix = np.random.randint(2, size=(5, 7))
+	#set the block values as 1 in the matrix. 
+	matrix[0][4] = 1
+	matrix[2][1] = 1
+	matrix[2][4] = 1
+	matrix[3][1] = 1
+	matrix[3][3] = 1
+	matrix[3][6] = 1
+	return matrix	
 
-#set the block values as 1 in the matrix. 
+def setUpCostMatrix():
+	global rows, columns
+	costMatrix = np.zeros((rows, columns))
+	return costMatrix
 
-matrix[0][4] = 1
-matrix[2][1] = 1
-matrix[2][4] = 1
-matrix[3][1] = 1
-matrix[3][3] = 1
-matrix[3][6] = 1
 
 def numOfPaths(matrix, costMatrix): 
 	for i in range(0, columns - 1):
@@ -30,8 +36,14 @@ def numOfPaths(matrix, costMatrix):
 				cost = costMatrix[i+1, j]
 			if matrix[i, j+1] == 0:
 				cost += costMatrix[i, j+1]
-
 			costMatrix[i, j] = cost
 	return costMatrix
 
-print numOfPaths(matrix, costMatrix)
+def main():
+
+	#if you want result of the above matrix, use the following:
+	#resultMatrix = numOfPaths(setUpMatrix(), setUpCostMatrix())
+	print numOfPaths(setUpMatrix(), setUpCostMatrix())
+
+if __name__ == '__main__':
+	main()
