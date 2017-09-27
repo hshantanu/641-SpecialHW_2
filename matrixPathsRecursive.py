0fa66cce909c35a313f96f35629b797e347b2795
spizler <aspizler@gmail.com>
2017-09-26 22:29:38 -0400

    wrote recursive method

--- matrixPathsRecursive.py
+++ matrixPathsRecursive.py
@@ -1,22 +1,58 @@
-import numpy
+import numpy as np
 
-# global variables
-rows = 0 #m
-cols = 0 #n
-M
+rows = 0 
+columns = 0 
+blockMatrix = None
+costMatrix = None
 
-def NumPaths(B, i, j):
-	if (i == rows - 1) and (j == cols - 1):
+def setUpBlockMatrix(blockPercent):
+	global rows, columns, blockMatrix
+	
+	#blockMatrix = np.zeros((rows, columns))
+	#blockMatrix = np.random.randint(2, size=(5, 7))
+
+	# Create blockMatrix with blockPercent random blocks
+	blockMatrix = np.random.randint(1/blockPercent, size=(rows, columns))
+	blockMatrix = blockMatrix < 1
+	# Set beginning and end to unblocked
+	blockMatrix[0][0] = False
+	blockMatrix[rows-1][columns-1] = False
+	return None	
+
+	
+def setUpCostMatrix():
+	global rows, columns, costMatrix
+	costMatrix = np.zeros((rows, columns))
+	costMatrix -= 1
+	return None
+
+def numPaths(i, j):
+	global rows, columns, blockMatrix, costMatrix
+
+	if (i == rows-1) and (j == columns-1) :
 		return 1
-
-	if (B[i][j] == True) or (i >= m) or (j >= n):
+		
+	if (i >= rows) or (j >= columns) or (blockMatrix[i][j] == True):
 		return 0
-
-	if M[i][j] < 0:
-		M[i][j] = NumPaths(B, i+1, j) + NumPaths(B, i, j+1)
+		
+	if costMatrix[i][j]<0:
+		costMatrix[i][j] = numPaths(i+1,j) + numPaths(i,j+1)
+		
+	return costMatrix[i][j]
 	
-	return M[i][j]
-
-def SolveNumPaths(B, rows, cols):
-	global M = [[-1] * cols] * rows
 	
+'''def solveNumPaths(B,rows,cols):
+	M = [[-1] * cols] * rows'''
+	
+def main():
+	global rows, columns
+
+	rows = 5
+	columns = 5
+	setUpBlockMatrix(0.1)
+	setUpCostMatrix()
+	
+	print numPaths(0, 0)
+
+if __name__ == '__main__':
+		main()
